7e66bfa3109c592e02612be9474cbfc6ce9b1f46
Zhen.Zhang <zhen.zhang.jrvs.ca@gmail.com>
2019-06-24 18:57:01 +0000

    update host_info.sh

--- scripts/host_info.sh
+++ scripts/host_info.sh
@@ -60,7 +60,7 @@ insert_stmt=$(cat <<-END
 INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, "timestamp") VALUES ('${hostname}', ${cpu_number}, '${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${L2_cache}, ${total_mem}, '${timestamp}');
 END
 )
-echo insert_stmt
+echo $insert_stmt
 
 #Step3:Execute INSERT statement
 export PGPASSWORD=$password
