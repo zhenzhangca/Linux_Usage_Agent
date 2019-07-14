ce0980a6d54117cd79dcbe5a1fc8d1f13c10d5eb
Zhen.Zhang <zhen.zhang.jrvs.ca@gmail.com>
2019-07-14 21:59:07 +0000

    update solutions.sql file

--- solutions.sql
+++ solutions.sql
@@ -1,5 +1,13 @@
 --query 1
-select i.cpu_number,u.host_id, i.total_mem from host_info as i, host_usage as u  where i.id = u.host_id order by i.total_mem desc;
+
+SELECT i.cpu_number, 
+       u.host_id, 
+       i.total_mem 
+FROM   host_info AS i, 
+       host_usage AS u 
+WHERE  i.id = u.host_id 
+ORDER  BY i.total_mem DESC; 
+
 --query 2
 select u.host_id, i.hostname, i.total_mem,(i.total_mem-u.memory_free)*100/i.total_mem as used_memory_percentage from host_info i, host_usage u where i.id=u.host_id; 
 
