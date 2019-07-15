02f02c9748fa3e7d644510d4ea29938945990725
Zhen.Zhang <zhen.zhang.jrvs.ca@gmail.com>
2019-07-15 00:00:00 +0000

    update solutions.sql file

--- solutions.sql
+++ solutions.sql
@@ -9,5 +9,20 @@ WHERE  i.id = u.host_id
 ORDER  BY i.total_mem DESC; 
 
 --query 2
-select u.host_id, i.hostname, i.total_mem,(i.total_mem-u.memory_free)*100/i.total_mem as used_memory_percentage from host_info i, host_usage u where i.id=u.host_id; 
-
+CREATE 
+OR 
+replace FUNCTION round_minutes(timestamp without time zone, integer) 
+returns timestamp without time zone AS $$
+SELECT 
+     Date_trunc('hour', $1) + Cast(($2::varchar ||'min') AS INTERVAL) 
+     * Round((Date_part('minute',$1)::float + Date_part('second',$1)/ 60.)::float / $2::float))
+     $$ language sql immutable;
+SELECT
+     id, total_mem, avg(((total_mem/1000.0) - memory_free)*100 / (total_mem/1000)) AS "prcnt", round_minutes(u.timestamp, 5) AS "interval" 
+FROM       host_info i 
+INNER JOIN host_usage u 
+ON         i.id = u.host_id 
+GROUP BY   id, 
+           total_mem, 
+           "interval" 
+ORDER BY   "interval" ;
